34a093533dc91ff966a1943bbf92507ce55f0c23
a-man010 <dl2s6871>
2018-04-28 17:32:00 +0530

    Initial Commit

---
+++ MapDirectionsTests/MapDirectionsTests.swift
@@ -0,0 +1,36 @@
+//
+//  MapDirectionsTests.swift
+//  MapDirectionsTests
+//
+//  Created by Amandeep Singh on 28/04/18.
+//  Copyright © 2018 Amandeep Singh. All rights reserved.
+//
+
+import XCTest
+@testable import MapDirections
+
+class MapDirectionsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
